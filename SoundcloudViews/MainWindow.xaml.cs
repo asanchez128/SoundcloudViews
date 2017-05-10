4149851ebab62ec4b36050c68d5e377b40037776
Scobiform <uwe@scobiform.com>
2017-05-10 03:01:13 +0200

    MenuHandler integrated

--- SoundcloudViews/MainWindow.xaml.cs
+++ SoundcloudViews/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace SoundcloudViews
         private RequestHandler requestHandler;
 
         //Default Browser URL
-        string defaultSongUrl = "https://soundcloud.com/harry-morgan-plays/get-more-soundcloud-plays";
+        string defaultSongUrl = "https://soundcloud-views.tk";
 
         //Async Task Cancellation
         CancellationTokenSource tokenSource = new CancellationTokenSource();
@@ -138,6 +138,10 @@ namespace SoundcloudViews
             SoundcloudViewsInput.Text = defaultSongUrl;
 
             firstStart = true;
+
+
+            Browser.MenuHandler = new MenuHandler();
+
 
             //Mainframe finished loading
             Browser.FrameLoadEnd += (sender, args) =>
@@ -273,6 +277,92 @@ namespace SoundcloudViews
         private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
         {
             Trace.WriteLine("LOADING");
+        }
+
+        #endregion
+
+        #region //Menuhandler - YOU CAN ADJUST THE FUCKING MENU HERE
+        private class MenuHandler : IContextMenuHandler
+        {
+            public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+            {
+                model.Clear();
+
+            }
+
+            public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+            {
+                return false;
+            }
+
+            public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+            {
+                //var chromiumWebBrowser = (ChromiumWebBrowser)browserControl;
+
+                //chromiumWebBrowser.Dispatcher.Invoke(() =>
+                //{
+                //    chromiumWebBrowser.ContextMenu = null;
+                //});
+            }
+
+            public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+            {
+                return false;
+
+                //var chromiumWebBrowser = (ChromiumWebBrowser)browserControl;
+
+                ////IMenuModel is only valid in the context of this method, so need to read the values before invoking on the UI thread
+                //var menuItems = GetMenuItems(model);
+
+                //chromiumWebBrowser.Dispatcher.Invoke(() =>
+                //{
+                //    var menu = new ContextMenu
+                //    {
+                //        IsOpen = true
+                //    };
+
+                //    RoutedEventHandler handler = null;
+
+                //    handler = (s, e) =>
+                //    {
+                //        menu.Closed -= handler;
+
+                //        //If the callback has been disposed then it's already been executed
+                //        //so don't call Cancel
+                //        if(!callback.IsDisposed)
+                //        { 
+                //            callback.Cancel();
+                //        }
+                //    };
+
+                //    menu.Closed += handler;
+
+                //    foreach (var item in menuItems)
+                //    {
+                //        menu.Items.Add(new MenuItem
+                //        {
+                //            Header = item.Item1,
+                //            Command = new RelayCommand(() => { callback.Continue(item.Item2, CefEventFlags.None); })
+                //        });
+                //    }
+                //    chromiumWebBrowser.ContextMenu = menu;
+                //});
+
+                //return true;
+            }
+
+            private static IEnumerable<Tuple<string, CefMenuCommand>> GetMenuItems(IMenuModel model)
+            {
+                var list = new List<Tuple<string, CefMenuCommand>>();
+                for (var i = 0; i < model.Count; i++)
+                {
+                    var header = model.GetLabelAt(i);
+                    var commandId = model.GetCommandIdAt(i);
+                    list.Add(new Tuple<string, CefMenuCommand>(header, commandId));
+                }
+
+                return list;
+            }
         }
 
         #endregion
